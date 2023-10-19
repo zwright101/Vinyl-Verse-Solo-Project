9d7a187e705752df542c2512bfc281f240d1bc4f
Zach Wright <zwright101@gmail.com>
2023-10-18 19:41:28 -0500

    Edited some of the text on the landing page to more fit my quick description for the application

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -6,7 +6,7 @@ import './LandingPage.css';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+  const [heading, setHeading] = useState('Welcome to the Verse!');
   const history = useHistory();
 
   const onLogin = (event) => {
@@ -20,42 +20,29 @@ function LandingPage() {
       <div className="grid">
         <div className="grid-col grid-col_8">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
+            Vinyl verse is a social media app designed for the lovers of music and collectors
+            of vinyl records. After signing up, jump into the Verse and begin adding your records
+            to your collection! 
           </p>
 
           <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
+            Once you're signed in and ready to add, navigate to the Add a Record page 
+            where you'll be able to search for your record and add it to your own digital collection
+            to display to your friends and other lovers of music! If there isn't an album for you to quick add, 
+            don't worry! You'll be able to manually enter a record to your collection using a couple of the 
+            key fields like artist and album name and submit it! If you come across a user or have a friend who shares similar music tastes, 
+            feel free to let them know by commenting on some of their albums by clicking the record in their collection and leaving a comment or recommendation! 
           </p>
 
           <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
+            
           </p>
         </div>
         <div className="grid-col grid-col_4">
           <RegisterForm />
 
           <center>
-            <h4>Already a Member?</h4>
+            <h4>Already in the Verse?</h4>
             <button className="btn btn_sizeSm" onClick={onLogin}>
               Login
             </button>
