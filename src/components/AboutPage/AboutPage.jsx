936264b2792ed74821aa160c3f8fff46ad546dd3
Zach Wright <zwright101@gmail.com>
2023-10-18 19:45:14 -0500

    Added some placeholder text to the about page. This will get edited a little further and designed with MUI later on

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -9,7 +9,20 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>
+            Vinyl verse is a social media app designed for the lovers of music and collectors
+            of vinyl records. After signing up, jump into the Verse and begin adding your records
+            to your collection!
+        </p>
+
+        <p>
+            Once you're signed in and ready to add, navigate to the Add a Record page 
+            where you'll be able to search for your record and add it to your own digital collection
+            to display to your friends and other lovers of music! If there isn't an album for you to quick add, 
+            don't worry! You'll be able to manually enter a record to your collection using a couple of the 
+            key fields like artist and album name and submit it! If you come across a user or have a friend who shares similar music tastes, 
+            feel free to let them know by commenting on some of their albums by clicking the record in their collection and leaving a comment or recommendation! 
+          </p>
       </div>
     </div>
   );
